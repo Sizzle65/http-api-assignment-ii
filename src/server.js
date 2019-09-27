ef93b266504596ebb7ee914c99ff8ca5d7de0b1d
Sam Belisle <spb6892@rit.edu>
2019-09-27 10:57:56 -0400

    Finished assignment

---
+++ src/server.js
@@ -0,0 +1,91 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// handle POST requests
+const handlePost = (request, response, parsedUrl) => {
+  // if post is to /addUser (our only POST url)
+  if (parsedUrl.pathname === '/addUser') {
+    const res = response;
+
+    // uploads come in as a byte stream that we need
+    // to reassemble once it's all arrived
+    const body = [];
+
+    // if the upload stream errors out, just throw a
+    // a bad request and send it back
+    request.on('error', (err) => {
+      console.dir(err);
+      res.statusCode = 400;
+      res.end();
+    });
+
+    // on 'data' is for each byte of data that comes in
+    // from the upload. We will add it to our byte array.
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    // on end of upload stream.
+    request.on('end', () => {
+      // combine our byte array (using Buffer.concat)
+      // and convert it to a string value (in this instance)
+      const bodyString = Buffer.concat(body).toString();
+      // since we are getting x-www-form-urlencoded data
+      // the format will be the same as querystrings
+      // Parse the string into an object by field name
+      const bodyParams = query.parse(bodyString);
+
+      // pass to our addUser function
+      jsonHandler.addUser(request, res, bodyParams);
+    });
+  }
+};
+
+// handle GET requests
+const handleGet = (request, response, parsedUrl) => {
+    // route to correct method based on url
+    if (parsedUrl.pathname === '/style.css') {
+        htmlHandler.getCSS(request, response);
+    } else if (parsedUrl.pathname === '/') {
+        htmlHandler.getIndex(request, response);
+    }  else if (parsedUrl.pathname === '/getUsers') {
+        jsonHandler.getUsers(request, response);
+    } else if (parsedUrl.pathname === '/notReal') {
+        jsonHandler.notReal(request, response);
+    }
+    else{
+        jsonHandler.notFound(request, response);
+    }
+};
+
+const handleHead = (request, response, parsedUrl) => {
+    if (parsedUrl.pathname === '/getUsers') {
+        jsonHandler.getUsersMeta(request, response);
+    }
+    else {
+        jsonHandler.notRealMeta(request, response);
+    }
+};
+
+const onRequest = (request, response) => {
+    console.log(request.url);
+    const parsedUrl = url.parse(request.url);
+
+    if (request.method === 'POST') {
+        handlePost(request, response, parsedUrl);
+    } else if(request.method === 'GET'){
+        handleGet(request, response, parsedUrl);
+    }
+    else {
+       handleHead(request, response, parsedUrl);
+    }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
